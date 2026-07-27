18ae5eb3158e245289158b020caf334a2e556019
samuel-garmany <65299214+samuel-garmany@users.noreply.github.com>
2026-07-27 02:24:34 -0600

    remove disk workaround from gnome

--- modules/hosts/desktop/configuration.nix
+++ modules/hosts/desktop/configuration.nix
@@ -1,22 +1,19 @@
 {
-  config.nixos.desktop =
-    {
-      config,
-      pkgs,
-      lib,
-      ...
-    }:
-    {
+  config.nixos.desktop = {
+    config,
+    pkgs,
+    lib,
+    ...
+  }: {
+    networking.hostName = "desktop"; # Define your hostname.
 
-      networking.hostName = "desktop"; # Define your hostname.
+    # Hide extraneous disks
+    #services.udev.extraRules = ''
+    #  # Hide specific encrypted partitions from Dolphin sidebar
+    #  ENV{ID_FS_UUID}=="5a37508d-66a3-40ba-a228-cdeb5606e521", ENV{UDISKS_IGNORE}="1"
+    #  ENV{ID_FS_UUID}=="d57a23be-cf31-405e-ac09-9cb06e6331c1", ENV{UDISKS_IGNORE}="1"
+    #'';
 
-      # Hide extraneous disks
-      services.udev.extraRules = ''
-        # Hide specific encrypted partitions from Dolphin sidebar
-        ENV{ID_FS_UUID}=="5a37508d-66a3-40ba-a228-cdeb5606e521", ENV{UDISKS_IGNORE}="1"
-        ENV{ID_FS_UUID}=="d57a23be-cf31-405e-ac09-9cb06e6331c1", ENV{UDISKS_IGNORE}="1"
-      '';
-
-      system.stateVersion = "26.05"; # Did you read the comment?
-    };
+    system.stateVersion = "26.05"; # Did you read the comment?
+  };
 }
