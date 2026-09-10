ff3f9fb2553bd85ff6bb6e6ab0f6fac985cbc8c9
Samuel Garmany <65299214+samuel-garmany@users.noreply.github.com>
2026-09-10 01:40:13 -0600

    fix fprintd on boot

--- nixos/features/desktop/login.nix
+++ nixos/features/desktop/login.nix
@@ -12,6 +12,7 @@
     };
 
     security.pam.services.login.fprintAuth = false;
+    security.pam.services.greetd.fprintAuth = false;
     security.pam.services.noctalia = {};
 
     # Note that to start plasma-polkit-agent with systemd on Fedora, you'll need
